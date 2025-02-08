ea899047a2610be6234d3c027853cd6c729812c6
Tahmid Ul Haque Choudhury <tahmidchoudhury019@gmail.com>
2025-02-08 12:12:14 +0530

    fix

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pickle
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -15,5 +16,8 @@ def predict():
     prediction = model.predict([data])
     return jsonify({'prediction': float(prediction[0])})
 
+    
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host="0.0.0.0", port=port)
